c2f0e29a07536a0334d9b0aed995b88c12d481b1
Mikhail Trishchenkov <kriomant@gmail.com>
2019-01-03 16:20:28 +0700

    Publish ogrep-rs 0.4.0

--- ogrep-rs.rb
+++ ogrep-rs.rb
@@ -1,8 +1,8 @@
 class OgrepRs < Formula
   desc "Outline grep — search in indentation-structured texts (Rust version)"
   homepage "https://github.com/kriomant/ogrep-rs"
-  url "https://github.com/kriomant/ogrep-rs/archive/0.3.0.tar.gz"
-  sha256 "b7d150ac08695548569f1c343fddc8d64262f508b63ab5b8aa928ed431be7703"
+  url "https://github.com/kriomant/ogrep-rs/archive/0.4.0.tar.gz"
+  sha256 "4f5537b30c4f00488be94704f4514e19cfc1b3482af5e1a0c0a93b5aeffaffa4"
   head "https://github.com/kriomant/ogrep-rs.git"
 
   depends_on "rust" => :build
